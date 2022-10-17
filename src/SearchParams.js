5a4a638c2049149d55325a2fd9fa1bd89f20c712
Thiago Brandão <194487+thiagobrandam@users.noreply.github.com>
2022-10-17 09:00:41 -0300

    Custom Hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 const ANIMALS = ["cat", "bird", "dog", "reptile", "rabbit"];
 
 const SearchParams = () => {
@@ -8,6 +8,21 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const breeds = [];
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -65,6 +80,14 @@ const SearchParams = () => {
           </select>
         </label>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
